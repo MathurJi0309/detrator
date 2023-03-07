99f5cfb041d1a5f593eeb591180b5e6c681c4907
MathurJi <anileshjjssacademy84@gmail.com>
2023-03-07 17:16:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const[posts,setPosts]= useState("");
+  useEffect
+  return (
+    <div className="App">
+      <h1>Hi i am here</h1>
+    </div>
+  );
+}
+
+export default App;
